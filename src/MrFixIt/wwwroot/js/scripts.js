0ff3d12efef3981a4eb916ed7a419d0ea63a925e
Sam Kirsch <samdkirsch2@gmail.com>
2017-05-05 13:12:50 -0400

    finish ajax route for claiming job

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,11 +1,24 @@
 ﻿$(document).ready(function(){
-  $("#claim-job").click(function() {
+  $("#claim-form").submit(function(event) {
+    event.preventDefault();
     var jobData = {};
     var dataArray = (($(this).serializeArray()));
-    console.log(dataArray);
+    var urlAction = $(this).data('url-action');
     for (let i = 0; i < dataArray.length; i++) {
         jobData[dataArray[i].name] = dataArray[i].value;
     }
-    console.log(jobData);
+    $.ajax({
+      url: urlAction,
+      type: 'POST',
+      datatype: 'json',
+      data: jobData,
+      success: function(result) {
+        console.log("success");
+        console.log(urlAction);
+      },
+      error: function(result) {
+        console.log("error");
+      }
+    })
   })
 })
