2c3b894372126dd56984af631c277a7dab274a7c
Sam Kirsch <samdkirsch2@gmail.com>
2017-05-05 13:26:05 -0400

    make the claim button hide when successfully clicked

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -4,6 +4,9 @@
     var jobData = {};
     var dataArray = (($(this).serializeArray()));
     var urlAction = $(this).data('url-action');
+    var errorbtn = $('.claimed-error');
+    var successbtn = $('.claimed-tag');
+    var claimbtn = $('.claim-button');
     for (let i = 0; i < dataArray.length; i++) {
         jobData[dataArray[i].name] = dataArray[i].value;
     }
@@ -13,11 +16,12 @@
       datatype: 'json',
       data: jobData,
       success: function(result) {
-        console.log("success");
-        console.log(urlAction);
+        claimbtn.addClass("hide");
+        successbtn.removeClass("hide");
       },
       error: function(result) {
-        console.log("error");
+        claimbtn.addClass("hide");
+        errorbtn.removeClass("hide");
       }
     })
   })
